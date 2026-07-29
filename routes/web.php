487671edc91b50ce13cbb9996ff23abb837e5e5a
Abdoulaye Fall <abdoulayef032@gmail.com>
2026-07-29 05:18:52 +0200

    Redirige la page d'accueil vers login ou dashboard selon connexion

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UtilisateurController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return auth()->check() ? redirect()->route('dashboard') : redirect()->route('login');
 });
 
 Route::middleware('auth')->group(function () {
