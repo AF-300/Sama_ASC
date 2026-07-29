b8c0401de0e621ad71495e752a107ad37ba56531
Abdoulaye Fall <abdoulayef032@gmail.com>
2026-07-29 05:32:07 +0200

    Corrige le debordement du menu mobile en le rendant defilable

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -211,7 +211,8 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden bg-nuit-dakar">
+    <!-- Responsive Navigation Menu -->
+<div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden bg-nuit-dakar max-h-[calc(100vh-5rem)] overflow-y-auto">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
